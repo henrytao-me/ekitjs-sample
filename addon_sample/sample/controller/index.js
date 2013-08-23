32c4d9a59a65ac60c6175e782c3442709d878867
Henry Tao <hi@henrytao.me>
2013-08-23 20:51:25 +0700

    init

--- addon_sample/sample/controller/index.js
+++ addon_sample/sample/controller/index.js
@@ -1,12 +1,19 @@
 var path = require('path');
 
-module.exports = function(instance) {
-	instance.sample.cIndex = instance.base.controller.extend({
+module.exports = function(instance, def) {
+	
+	instance.doc.cIndex = instance.base.controller.extend({
 		'*': function(req, res, next){
 			res.render(path.join(__dirname, '..', 'static', 'view', 'index'), {
-				css: instance._config.asset.renderTags('css'),
-				js: instance._config.asset.renderTags('js')
-			});
+				css: def.asset.renderTags('css'),
+				js: def.asset.renderTags('js')
+			});			
+		},
+		'get://hellomoto': function(req, res, next){
+			res.send('hellomoto');
+		},
+		'get://hellomoto/:hi': function(req, res, next){
+			res.send(req.params.hi);
 		}
 	});
 };
