80ecc0b8d94abf75c6e917eec3abde8bf67a7c64
Henry Tao <hi@henrytao.me>
2013-08-25 13:36:35 +0700

    init

--- node_modules/ekitjs/app.js
+++
@@ -1,9 +0,0 @@
-var ekitjs = require('ekitjs');
-var path = require('path');
-
-// set config file
-ekitjs.set('root_path', __dirname);
-ekitjs.set('config.json', path.join(__dirname, 'config.json'));
-
-// start server
-ekitjs.start();
